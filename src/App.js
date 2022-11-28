54e2a4ee6884132a97a9e9b9cd69422bec9ef5ee
Joel Dario <joeldario9912@gmail.com>
2022-11-28 19:13:35 -0300

    comence la pagina de bienvenida

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='titulo'>focus</h1>
+      <p className='bienvenida'>Bienvenido! Ayudanos a darte una mejor experiencia</p>
+      <div className='btn-portada'>Fotógrafo</div>
+      <div className='btn-portada'>Espectador</div>
     </div>
   );
 }
