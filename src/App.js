9d41aa9a9c2287254b6515a1c9375f66da4a0f92
Joel Dario <joeldario9912@gmail.com>
2022-11-29 05:19:11 -0300

    agregue animacion al icono

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
+import { MdCamera } from "react-icons/md";
 
 function App() {
   return (
     <div className="App">
-      <h1 className='titulo'>focus</h1>
+      <h1 className='titulo'>f<MdCamera className='icono' />cus</h1>
       <p className='bienvenida'>Bienvenido! Ayudanos a darte una mejor experiencia</p>
       <div className='btn-portada'>Fotógrafo</div>
       <div className='btn-portada'>Espectador</div>
     </div>
   );
 }
+
+
 
 export default App;
